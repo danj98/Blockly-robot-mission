b71b4e784e45976d68cdd484946db090d34d3985
Maciek Kociela <kocielam@gmail.com>
2021-02-09 18:54:09 +0100

    Clear page

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <BlocklyComponent id="blockly1">
-      <block type="controls_ifelse"></block>
-      <block type="logic_compare"></block>
-      <block type="logic_operation"></block>
-      <block type="controls_repeat_ext">
-          <value name="TIMES">
-              <shadow type="math_number">
-                  <field name="NUM">10</field>
-              </shadow>
-          </value>
-      </block>
-      <block type="logic_operation"></block>
-      <block type="logic_negate"></block>
-      <block type="logic_boolean"></block>
-      <block type="logic_null" disabled="true"></block>
-      <block type="logic_ternary"></block>
-      <block type="text_charAt">
-          <value name="VALUE">
-              <block type="variables_get">
-                  <field name="VAR">text</field>
-              </block>
-          </value>
-      </block>
-    </BlocklyComponent>
-
     <BlocklyComponent id="blockly2" :options="options" ref="foo"></BlocklyComponent>
     <p id="code">
       <button v-on:click="showCode()">Show JavaScript</button>
@@ -35,54 +9,29 @@
 </template>
 
 <script>
-/**
- * @license
- * 
- * Copyright 2019 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+import BlocklyComponent from "./components/BlocklyComponent.vue";
+import "./blocks/stocks";
+import "./prompt";
 
-/**
- * @fileoverview Main Vue component that includes the Blockly component.
- * @author samelh@google.com (Sam El-Husseini)
- */
-
-import BlocklyComponent from './components/BlocklyComponent.vue'
-import './blocks/stocks';
-import './prompt';
-
-import BlocklyJS from 'blockly/javascript';
+import BlocklyJS from "blockly/javascript";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    BlocklyComponent
+    BlocklyComponent,
   },
-  data(){
+  data() {
     return {
-      code: '',
+      code: "",
       options: {
-        media: 'media/',
-        grid:
-          {
-            spacing: 25,
-            length: 3,
-            colour: '#ccc',
-            snap: true
-          },
-        toolbox:
-        `<xml>
+        media: "media/",
+        grid: {
+          spacing: 25,
+          length: 3,
+          colour: "#ccc",
+          snap: true,
+        },
+        toolbox: `<xml>
           <category name="Logic" colour="%{BKY_LOGIC_HUE}">
             <block type="controls_if"></block>
             <block type="logic_compare"></block>
@@ -119,27 +68,28 @@ export default {
             <block type="stock_buy_prog"></block>
             <block type="stock_fetch_price"></block>
           </category>
-        </xml>`
-      }
-    }
+        </xml>`,
+      },
+    };
   },
   methods: {
     showCode() {
       this.code = BlocklyJS.workspaceToCode(this.$refs["foo"].workspace);
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
 }
 
-html, body {
+html,
+body {
   margin: 0;
 }
 
@@ -148,17 +98,9 @@ html, body {
   right: 0;
   bottom: 0;
   width: 50%;
-  height: 50%;
+  height: 100%;
   margin: 0;
   background-color: beige;
-}
-
-#blockly1 {
-  position: absolute;
-  right: 0;
-  top: 0;
-  width: 50%;
-  height: 50%;
 }
 
 #blockly2 {
@@ -166,6 +108,6 @@ html, body {
   left: 0;
   bottom: 0;
   width: 50%;
-  height: 50%;
+  height: 100%;
 }
 </style>
