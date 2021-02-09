3668a5d590f530cef06e83270f104f3e8b6f7c58
Maciek Kociela <kocielam@gmail.com>
2021-02-09 19:32:28 +0100

    el

--- src/blocks/element.js
+++ src/blocks/element.js
@@ -1,35 +1,31 @@
 import * as Blockly from "blockly/core";
-
 Blockly.Blocks["element"] = {
   init: function () {
-    this.appendValueInput("NAME")
-      .setCheck(null)
-      .appendField("element")
-      .appendField(
-        new Blockly.FieldDropdown([
-          ["class", "."],
-          ["id", "#"]
-        ]),
-        "element"
-      )
-      .appendField(new Blockly.FieldTextInput("class-name"), "NAME");
-    this.setOutput(true, "String");
+    this.appendValueInput("Number")
+      .setCheck("Number")
+      .appendField("Buy Stock ID")
+      .appendField(new Blockly.FieldNumber(0), "ID")
+      .appendField("For amount")
+      .appendField(new Blockly.FieldNumber(0), "Amount")
+      .appendField("At Price")
+      .appendField(new Blockly.FieldNumber(0), "Price");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, "String");
     this.setColour(230);
-    this.setTooltip("");
-    this.setHelpUrl("");
+    this.setTooltip("buy id");
+    this.setHelpUrl("https://example.com");
   }
 };
 
 Blockly.JavaScript["element"] = function (block) {
-  var dropdown_element = block.getFieldValue("element");
-  var text_name = block.getFieldValue("NAME");
-  var value_name = Blockly.JavaScript.valueToCode(
+  var number_id = block.getFieldValue("ID");
+  var number_amount = block.getFieldValue("Amount");
+  var number_price = block.getFieldValue("Price");
+  var value_number = Blockly.JavaScript.valueToCode(
     block,
-    "NAME",
+    "Number",
     Blockly.JavaScript.ORDER_ATOMIC
   );
-  // TODO: Assemble JavaScript into code variable.
-  var code = `${dropdown_element}${text_name}`;
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
+  var code = `hello(${number_id},${number_amount},${number_price},${value_number});\n`;
+  return code;
 };
