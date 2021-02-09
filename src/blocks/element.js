3914830abf4cbec72b1a911ecc6f84bc882f3090
Maciek Kociela <kocielam@gmail.com>
2021-02-09 19:41:26 +0100

    3

--- src/blocks/element.js
+++ src/blocks/element.js
@@ -2,6 +2,39 @@ import * as Blockly from "blockly/core";
 
 Blockly.Blocks["element"] = {
   init: function () {
+    this.appendValueInput("Number")
+      .setCheck("Number")
+      .appendField("Add Class")
+      .appendField(new Blockly.FieldNumber(0), "ID")
+      .appendField("For amount")
+      .appendField(new Blockly.FieldNumber(0), "Amount")
+      .appendField("At Price")
+      .appendField(new Blockly.FieldNumber(0), "Price");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, "String");
+    this.setColour(230);
+    this.setTooltip("buy id");
+    this.setHelpUrl("https://example.com");
+  }
+};
+
+Blockly.JavaScript["element"] = function (block) {
+  var number_id = block.getFieldValue("ID");
+  var number_amount = block.getFieldValue("Amount");
+  var number_price = block.getFieldValue("Price");
+  var value_number = Blockly.JavaScript.valueToCode(
+    block,
+    "Number",
+    Blockly.JavaScript.ORDER_ATOMIC
+  );
+  var code = `hello(${number_id},${number_amount},${number_price},${value_number});\n`;
+  return code;
+};
+
+/*
+
+Blockly.Blocks["element"] = {
+  init: function () {
     this.appendValueInput("NAME")
       .setCheck(null)
       .appendField("element")
@@ -34,3 +67,4 @@ Blockly.JavaScript["element"] = function (block) {
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
+*/
