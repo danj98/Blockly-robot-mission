12b85bab85ec2af340f441e499a172d05f00202c
h579699 <jjonasson@ymail.com>
2023-05-02 14:06:45 +0200

    Flow blocks

--- src/blocks/flow.js
+++ src/blocks/flow.js
@@ -1,48 +1,55 @@
 import * as Blockly from "blockly/core";
 
-// Block for parallel actions
-Blockly.Blocks['parallel_actions'] = {
+/**
+ *  Block for parallel actions
+ */
+Blockly.Blocks['fork'] = {
     init: function() {
-      this.appendDummyInput()
-          .appendField("Parallel actions");
-      this.appendStatementInput("actions1")
-          //.setCheck("action")
-          .appendField("Actions 1:");
-      this.appendStatementInput("actions2")
-          //.setCheck("action")
-          .appendField("Actions 2:");
+      this.appendStatementInput("Parallel tasks")
+          .setCheck(null)
+          .appendField("Fork");
       this.setPreviousStatement(true, null);
       this.setNextStatement(true, null);
       this.setColour(120);
    this.setTooltip("Allows actions in parallel");
    this.setHelpUrl("");
     }
-  };
+};
 
-  Blockly.JavaScript['parallel_actions'] = function(block) {
-    var statements_actions1 = Blockly.JavaScript.statementToCode(block, 'actions1');
-    var statements_actions2 = Blockly.JavaScript.statementToCode(block, 'actions2');
-    var code = '{ "parallel_actions": [\n' + statements_actions1 + ',\n' + statements_actions2 + '\n]\n}\n';
+Blockly.JavaScript['fork'] = function(block) {
+    var statements_parallel_tasks = Blockly.JavaScript.statementToCode(block, 'Parallel tasks');
+    // TODO: Assemble JavaScript into code variable.
+    var code = '{\n'
+        + '"parallel_tasks": ['
+        + statements_parallel_tasks 
+        + ']'
+        + '}\n'
     return code;
-  };
+};
 
-// Block for a sequence of actions
+/**
+ * Block for a sequence of actions
+ */
 Blockly.Blocks['sequence'] = {
     init: function() {
-      this.appendDummyInput()
+      this.appendStatementInput("Sequence")
+          .setCheck(null)
           .appendField("Sequence");
-      this.appendStatementInput("sequence")
-          .setCheck("Action");
-      this.setPreviousStatement(true, ["Action", "Flow"]);
-      this.setNextStatement(true, ["Action", "Flow"]);
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
       this.setColour(120);
-   this.setTooltip("Allows a sequence of actions as input");
+   this.setTooltip("");
    this.setHelpUrl("");
     }
-  };
+};
 
 Blockly.JavaScript['sequence'] = function(block) {
-    var statements_sequence = Blockly.JavaScript.statementToCode(block, 'sequence');
-    var code = '{ "sequence": [\n' + statements_sequence + '\n]\n}\n';
+    var statements_sequence = Blockly.JavaScript.statementToCode(block, 'Sequence');
+    // TODO: Assemble JavaScript into code variable.
+    var code = '{\n'
+        + '"task_sequence": ['
+        + statements_sequence 
+        + ']'
+        + '}\n'
     return code;
-  };
+};
